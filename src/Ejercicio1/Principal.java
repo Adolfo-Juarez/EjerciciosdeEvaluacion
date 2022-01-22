982fcf0ddef82336e07cc13423bf548e3e409b70
Adolfo-Juarez <213358@ids.upchiapas.edu.mx>
2022-01-22 16:07:36 -0600

    0.1.0

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main (String[] Args){
+        
+        System.out.println("\t\t¿ES PRIMO?");
+        
         Scanner sc = new Scanner(System.in);
         Primo es = new Primo();
         int Num=1;
         while(Num!=0){
             System.out.print("Ingrese un número: ");
             Num = sc.nextInt();
-            System.out.println(Num+es.Primo(Num));
-            System.out.println("Agruegue el siguiente número o escriba 0 para salir\n");
+            if(Num!=0){ //Evita que este mensaje salga cuando se presiona 0 para salir
+                System.out.println(Num+es.Primo(Num));
+                System.out.println("Agruegue el siguiente número o escriba 0 para salir\n");
+            }
         }
     }
 }
